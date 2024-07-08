12f73a7d6ff709f69b1ecaef74c6bab6664b9060
Tariqul Islam <tarekislam1023@gmail.com>
2024-07-08 13:56:18 +0600

    Ques 1

--- prac.py
+++ prac.py
@@ -507,28 +507,28 @@
 # s1.get_avg()
 
 #Abstraction
-# class Account:
-#     def __init__(self, bal, acc):
-#         self.balance = bal
-#         self.account_no = acc
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
     
-#     def debit(self, amount):
-#         self.balance -= amount
-#         print("Tk.", amount, "was debited")
-#         print("Total Balance = ", self.get_balance())
+    def debit(self, amount):
+        self.balance -= amount
+        print("Tk.", amount, "was debited")
+        print("Total Balance = ", self.get_balance())
     
-#     def credit(self, amount):
-#         self.balance += amount
-#         print("Tk.", amount, "was credited.")
-#         print("Total Balance is", self.get_balance())
+    def credit(self, amount):
+        self.balance += amount
+        print("Tk.", amount, "was credited.")
+        print("Total Balance is", self.get_balance())
 
-#     def get_balance(self):
-#         return self.balance
+    def get_balance(self):
+        return self.balance
 
-# acc1 = Account(10000, 12345)
-# acc1.debit(5000)
-# acc1.credit(10000)
-# acc1.credit(15000)
+acc1 = Account(10000, 12345)
+acc1.debit(5000)
+acc1.credit(10000)
+acc1.credit(15000)
 
 #Private Public
 
